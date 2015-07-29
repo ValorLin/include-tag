931c9306c50f7bcd72f4ec28d57708d3ffb491a2
威老 <qqq123026689@126.com>
2015-07-29 19:21:51 +0800

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var wrapJs = require('gulp-wrap-js');
 
 gulp.task('browser', function () {
     return gulp.src([
             './libs/browser/path.js',
             './libs/browser/getFileContent.js',
-            './libs/includeTag.js'
+            './libs/includeTag.js',
+           './libs/browser/export.js'
         ])
         .pipe(concat('includeTag.browser.js'))
+        .pipe(wrapJs('(function(window){%= body %})(window)'))
         .pipe(gulp.dest('./dist/'));
 });
-
 gulp.task('node', function () {
     return gulp.src([
             './libs/node/path.js',
             './libs/node/getFileContent.js',
             './libs/includeTag.js',
-            './libs/node/exports.js'
+            './libs/node/export.js'
         ])
         .pipe(concat('includeTag.js'))
         .pipe(gulp.dest('./dist/'));
