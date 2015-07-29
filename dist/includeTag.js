237be81df4dfad9042df1f392b078d64ed094fb0
威老 <qqq123026689@126.com>
2015-07-29 19:02:11 +0800

    node exports

--- dist/includeTag.js
+++ dist/includeTag.js
@@ -16,3 +16,4 @@ function includeTag(cwd, contents) {
         }
     });
 }
+module.exports = includeTag;
--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,21 @@ var concat = require('gulp-concat');
 
 gulp.task('browser', function () {
     return gulp.src([
-        './libs/browser/path.js',
-        './libs/browser/getFileContent.js',
-        './libs/includeTag.js'
-    ])
+            './libs/browser/path.js',
+            './libs/browser/getFileContent.js',
+            './libs/includeTag.js'
+        ])
         .pipe(concat('includeTag.browser.js'))
         .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('node', function () {
     return gulp.src([
-        './libs/node/path.js',
-        './libs/node/getFileContent.js',
-        './libs/includeTag.js'
-    ])
+            './libs/node/path.js',
+            './libs/node/getFileContent.js',
+            './libs/includeTag.js',
+            './libs/node/exports.js'
+        ])
         .pipe(concat('includeTag.js'))
         .pipe(gulp.dest('./dist/'));
 });
